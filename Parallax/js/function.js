67aa452e83ee3d2ddbbbbeb5f277995154141f94
Armanway <armanway95@gmail.com>
2016-06-17 09:07:18 -0700

    file
    this file

---
+++ Parallax/js/function.js
@@ -0,0 +1,43 @@
+$(window).scroll(function() {
+
+    var wScroll = $(this).scrollTop();
+
+    $(".logo").css({
+        'transform': 'translate(0px, '+ wScroll /2 + '%)'
+    });
+
+    $(".back-bird").css({
+        'transform': 'translate(0px, '+ wScroll /4 + '%)'
+    });
+
+    $(".fore-bird").css({
+        'transform': 'translate(0px, -'+ wScroll /20 + '%)'
+    });
+
+        // offset artinya memperoses objeck yang di scroll 
+    if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+        $('.clothes-pics figure').each(function(i) {
+            // setTimeout artinya adalah Don't do anything
+            setTimeout(function() {
+                 $('.clothes-pics figure').eq(i).addClass('is-showing');
+            }, 150 * (i+1));
+        });
+    }
+ 
+    if(wScroll > $('.large-window').offset().top - $(window).height()) {
+        $('.large-window').css({'background-postion':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+
+        var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+
+        $('.window-tint').css({'opacity': opacity});
+    }
+
+    if(wScroll > $('.blog-posts').offset().top - $(window).height()) {
+
+        var offset = Math.min(0, wScroll - $('.blog-posts').offset().top + $(window).height() - 350);
+
+        $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+
+        $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    }
+});
